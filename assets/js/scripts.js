6e73a0f9d58c2af8d27e8ca29b2760675259c0e5
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-03-02 13:25:12 +0000

    Finished working on scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,29 +14,88 @@ startGame () {
     this.matchedCards = [];
     this.busy = true;
 
-    this.shuffleCards();
+    setTimeout(() => {
+        this.shuffleCards();
+        this.countDown = this.StartCountDown();
+        this.busy = false;
+    }, 500);
+    this.hideCards();
+    this.timer.innerText = this.timeRemaining;
+    this.ticker.innerText = this.totalClicks;
+}
+hideCards () {
+this.cardsArray.forEach(card => {
+card.classList.remove('visible');
+card.classList.remove('matched');
+});
 }
 flipCard(card) {
-     if(this.canFlipCard) {
+     if(this.canFlipCard(card)) {
          this.totalClicks++;
          this.ticker.innerText = this.totalClicks;
          card.classList.add('visible');
 
-        
+        if(this.cardToCheck)
+        this.checkForCardMatch(card);
+        else 
+        this.cardToCheck = card;
      }
+}
+checkForCardMatch(card) {
+ if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+ this.cardMatch(card, this.cardToCheck);
+else 
+this.cardMisMatch(card, this.cardToCheck);
+
+this.cardToCheck = null;
+}
+cardMatch(card1, card2) {
+this.matchedCards.push(card1);
+this.matchedCards.push(card2);
+card1.classList.add('matched');
+card2.classList.add('matched');
+if(this.matchedCards.length === this.cardsArray.length)
+this.winner();
+}
+cardMisMatch(card1, card2) {
+this.busy = true;
+setTimeout(() => {
+    card1.classList.remove('visible');
+    card2.classList.remove('visible');
+    this.busy = false;
+}, 1000);
+}
+getCardType(card) {
+return card.getElementsByClassName('card-value')[0].src;
+}
+StartCountDown() {
+    return setInterval(() => {
+        this.timeRemaining--;
+        this.timer.innerText = this.timeRemaining;
+        if(this.timeRemaining === 0)
+        this.gameOver();
+    }, 1000);
+}
+
+gameOver () {
+    clearInterval(this.countDown);
+    document.getElementById('game-over-text').classList.add('visible');
+}
+winner () {
+    clearInterval(this.countDown);
+    document.getElementById('winner-text').classList.add('visible')
 }
 
 shuffleCards() {
     for(let i = this.cardsArray.length -1; i > 0; i--) {
        let randIndex = Match.floor(Math.random() * (i+1));
-       this.cardsArray[randomIndex].style.order = i;
-       this.cardsArray[i].style.order = randomIndex;
+       this.cardsArray[randIndex].style.order = i;
+       this.cardsArray[i].style.order = randIndex;
     }
 }
 
 canFlipCard(card) {
-    return true;
-    //return !this.busy && !this.matchedCards.includes(cards) && card !== this.cardToCheck;
+    return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
 }
 }
 
@@ -48,13 +107,13 @@ function ready() {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
             game.startGame();
-        })
+        });
     });
     cards.forEach(card => {
         card.addEventListener('click', () => {
             game.flipCard(card);
-        })
-    })
+        });
+    });
 }
 
 if(document.readyState === 'loading') {
